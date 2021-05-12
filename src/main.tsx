a8220ad9b1558994c2628134f8b18150bf7169ce
anhtudo97 <anhtudo97@gmail.com>
2021-05-12 11:55:57 +0700

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from './core/app';
+import React from "react";
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>App</h1>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import dotEnv from "dotenv";
+
+// https://vitejs.dev/config/
+
+export default ({ mode }: any) => {
+  dotEnv.config({ path: `./.env` });
+  return defineConfig({
+    plugins: [reactRefresh()],
+    define: {
+      processEnv: process.env,
+    },
+  });
+};
