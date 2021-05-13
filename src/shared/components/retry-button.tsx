78603f0e0e49e2218b78e5b9d0ab7766853809d4
anhtudo97 <anhtudo97@gmail.com>
2021-05-13 21:32:45 +0700

    feat: retry button

---
+++ src/shared/components/retry-button.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Icon, IconButton, makeStyles } from "@material-ui/core";
+
+type RetryButtonProps = {
+  onClick: () => void;
+  message: string;
+};
+
+const RetryButton: React.FC<RetryButtonProps> = ({ onClick, message }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <IconButton onClick={onClick} data-testid="retry-button">
+        <Icon className={classes.icon}>refresh</Icon>
+      </IconButton>
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "auto",
+  },
+  icon: {
+    "&&": {
+      color: "black",
+      fontSize: "1.5em",
+    },
+  },
+  message: {
+    margin: "0 10px",
+    textAlign: "center",
+  },
+});
+
+export default RetryButton;
