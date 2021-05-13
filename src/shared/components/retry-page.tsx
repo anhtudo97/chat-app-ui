6413b84f4e25ec651ed5a5b83565bf9a6a3f7ef7
anhtudo97 <anhtudo97@gmail.com>
2021-05-13 21:55:10 +0700

    feat: retry page

---
+++ src/shared/components/retry-page.tsx
@@ -0,0 +1,27 @@
+import RetryButton from "./retry-button";
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+export type RetryPageProps = {
+  onRetry: () => void;
+  errorMessage: string;
+};
+
+const RetryPage: React.FC<RetryPageProps> = ({ onRetry, errorMessage }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <RetryButton onClick={onRetry} message={errorMessage} />
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default RetryPage;
