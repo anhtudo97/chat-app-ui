2d74f4fb8a28868c6c71ec959c44f2a429371090
anhtudo97 <anhtudo97@gmail.com>
2021-05-14 16:31:35 +0700

    chore: update signin

--- src/features/auth/data/sources/google-auth.ts
+++ src/features/auth/data/sources/google-auth.ts
@@ -7,4 +7,21 @@ export default class GoogleAuth {
       });
     });
   }
+
+  async signIn() {
+    return new Promise<string>((resolve, reject) => {
+      gapi.load("auth2", () => {
+        gapi.auth2
+          .init({
+            client_id: `${processEnv.VITE_GOOGLE_CLIENT}`,
+            scope: "profile",
+          })
+          .then(async (ga) => {
+            const x = await ga.signIn();
+            resolve(x.getAuthResponse().id_token);
+          })
+          .catch(reject);
+      });
+    });
+  }
 }
