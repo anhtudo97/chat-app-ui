1e9d351c835c726b048ebe1793847fff5fe7226f
anhtudo97 <anhtudo97@gmail.com>
2021-05-15 14:17:36 +0700

    feat: user types

---
+++ src/features/user/types/user-error.ts
@@ -0,0 +1,19 @@
+enum UserError {
+  general,
+  network,
+  notFound,
+  usernameTaken,
+}
+
+export const stringifyUserError = (error: UserError): string => {
+  switch (error) {
+    case UserError.network:
+      return "Check your network";
+    case UserError.notFound:
+      return "User not found";
+    case UserError.usernameTaken:
+      return "Username taken";
+    default:
+      return "Something went wrong";
+  }
+};
---
+++ src/features/user/types/user.ts
@@ -0,0 +1,35 @@
+export type Photo = {
+  source: string;
+  medium: string;
+  small: string;
+};
+
+export type User = {
+  id: string;
+  username: string;
+  name: string | null;
+  photo: Photo | null;
+};
+
+export type Me = User & {
+  activeStatus: boolean;
+};
+
+export type UserCreation = {
+  username: string;
+  name?: string;
+  photo?: File;
+};
+
+export type UserUpdate = {
+  username?: string;
+  name?: string;
+  deleteName?: boolean;
+  photo?: File;
+  deletePhoto?: boolean;
+};
+
+export type GetUserArgs = {
+  userID?: string;
+  username?: string;
+};
