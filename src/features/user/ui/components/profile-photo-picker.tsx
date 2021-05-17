e417c6bd9351c5b549b910632c750805fc40f619
anhtudo97 <anhtudo97@gmail.com>
2021-05-17 21:58:51 +0700

    feat: user profile photo picker component

---
+++ src/features/user/ui/components/profile-photo-picker.tsx
@@ -0,0 +1,187 @@
+import React, { useCallback, useState } from "react";
+import { Icon, makeStyles } from "@material-ui/core";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import { CircleButton } from "./circle-button";
+import { MenuDialog } from "./menu-dialog";
+import AlertDialog from "../../../../shared/components/alert-dialog";
+import { nonDraggable } from "../../../../shared/styles/shared";
+import { useDropzone } from "react-dropzone";
+
+type ProfilePhotoPickerStyle = {
+  size?: number;
+};
+
+type ProfilePhotoPickerProps = {
+  defaultSrc: string;
+  src?: string;
+  onPhotoPicker: (photo: File) => void;
+  onPhotoRemoved: () => void;
+} & ProfilePhotoPickerStyle;
+
+const useStyles = makeStyles<
+  Theme,
+  ProfilePhotoPickerStyle & { isDragActive: boolean }
+>((_) => {
+  const _defaultSize = 150;
+  return {
+    wrapper: (props) => {
+      const size = `${props.size ?? _defaultSize}px`;
+      return {
+        position: "relative",
+        width: size,
+        height: size,
+        outline: "none",
+      };
+    },
+    photo: (props) => {
+      const size = `${props.size ?? _defaultSize}px`;
+      return {
+        borderRadius: "50%",
+        border: "1px solid black",
+        width: size,
+        height: size,
+        outline: "none",
+        ...nonDraggable,
+      };
+    },
+    dragIndicator: (props) => {
+      const size = `${props.size ?? _defaultSize}px`;
+      const opacity = props.isDragActive ? 1 : 0;
+      return {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        top: 0,
+        borderRadius: "50%",
+        width: size,
+        height: size,
+        background: "rgb(0, 0, 0, 0.5)",
+        opacity: opacity,
+        transition: "200ms ease",
+      };
+    },
+    dragIcon: (props) => {
+      const size = `${(props.size ?? _defaultSize) / 3}px`;
+      return {
+        "&&": {
+          fontSize: size,
+          color: "white",
+        },
+      };
+    },
+    buttonWrapper: {
+      position: "absolute",
+      bottom: "5%",
+      right: "5%",
+    },
+  };
+});
+
+export const ProfilePhotoPicker: React.FC<ProfilePhotoPickerProps> = ({
+  defaultSrc,
+  src,
+  onPhotoPicker,
+  onPhotoRemoved,
+  size,
+}) => {
+  const [confirming, setConfirming] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const onDrop = useCallback((files: File[]) => {
+    if (files[0]) {
+      onPhotoPicker(files[0]);
+    }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    onDrop,
+    accept: "image/jpeg, image/png",
+    noClick: true,
+    noKeyboard: true,
+  });
+
+  const pickPhoto = useCallback(() => {
+    setMenuOpen(false);
+    open();
+  }, []);
+
+  const confirmRemoval = useCallback(() => {
+    setMenuOpen(false);
+    setConfirming(true);
+  }, []);
+
+  const removalCanceled = useCallback(() => {
+    setConfirming(false);
+  }, []);
+
+  const removalConfirmed = useCallback(() => {
+    setConfirming(false);
+    onPhotoRemoved();
+  }, []);
+
+  const openMenu = useCallback(() => {
+    setMenuOpen(true);
+  }, []);
+
+  const classes = useStyles({ size, isDragActive });
+
+  const menuItems = [
+    { icon: "edit", label: "New profile photo", onClick: confirmRemoval },
+  ];
+
+  if (src) {
+    menuItems.push({
+      icon: "clear",
+      label: "Remove current photo",
+      onClick: confirmRemoval,
+    });
+  }
+
+  const rootProps = getRootProps({
+    className: classes.dragIndicator,
+  });
+
+  const inputProps = getInputProps({
+    type: "file",
+    accept: "image/jpeg, image/png",
+    hidden: true,
+  });
+
+  return (
+    <div className={classes.wrapper} data-testid="profile-photo-picker">
+      <div {...rootProps} data-testid="profile-photo-drop-zone">
+        <Icon className={classes.dragIcon}>camera_alt</Icon>
+      </div>
+
+      <img
+        className={classes.photo}
+        src={src ?? defaultSrc}
+        alt="profile photo"
+      />
+      <div className={classes.buttonWrapper}>
+        <CircleButton
+          onClick={openMenu}
+          size={size ? size * 0.2 : undefined}
+          icon="edit"
+          bgColor="#212121"
+          iconColor="#ffffff"
+          data-testid="edit-profile-photo"
+        />
+      </div>
+      <input {...inputProps} data-testid="profile-photo-input" />
+      <AlertDialog
+        title="Remove photo"
+        content={"You're about to remove the selected photo"}
+        open={confirming}
+        onCancel={removalCanceled}
+        onConfirm={removalConfirmed}
+      />
+      <MenuDialog
+        visible={menuOpen}
+        items={menuItems}
+        onClose={() => setMenuOpen(false)}
+      />
+    </div>
+  );
+};
