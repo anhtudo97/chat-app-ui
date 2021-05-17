1cb9c0527c30bfc7715831251b867f7937bf3f38
anhtudo97 <anhtudo97@gmail.com>
2021-05-17 15:37:26 +0700

    update: redux store

--- src/core/redux/store-extra-arg.ts
+++ src/core/redux/store-extra-arg.ts
@@ -1,9 +1,12 @@
 import IAuthRepository from "../../features/auth/data/auth-repository";
 import { IMeRepository } from "../../features/user/data/me-repository";
+import { IFriendRepository } from "../../features/friend/data/friend-repository";
 
 type StoreExtraArg = {
   authRepo: IAuthRepository;
   meRepo: IMeRepository;
+
+  friendRepo: IFriendRepository;
 };
 
 export default StoreExtraArg;
