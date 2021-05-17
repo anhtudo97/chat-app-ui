b4f390e93f2be718288a48c3de0ecc67d6aaad35
anhtudo97 <anhtudo97@gmail.com>
2021-05-17 15:37:49 +0700

    feat: user friends button component

---
+++ src/features/user/ui/components/friends-button.tsx
@@ -0,0 +1,50 @@
+import { Button, makeStyles } from "@material-ui/core";
+import React, { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import { useAppSelector } from "../../../../core/redux/hooks";
+
+const useStyles = makeStyles({
+  wrapper: {
+    minHeight: "36px",
+    maxHeight: "36px",
+  },
+});
+const FriendsButton = () => {
+  const friends = useAppSelector((state) => state.friends.friends);
+  const history = useHistory();
+  const classes = useStyles();
+
+  const goToFriends = useCallback(async () => {
+    history.push("/friends", { canGoBack: true });
+  }, [history]);
+
+  const goToFriendsInAnotherTab = useCallback(async () => {
+    window.open("/friends");
+  }, [history]);
+
+  let child: React.ReactNode;
+  if (!friends) {
+    child = <div data-testid="no-button" />;
+  } else {
+    const count = friends.length;
+    const label = count
+      ? count === 1
+        ? "1 Friend"
+        : `${count} Friends`
+      : "No Friends";
+    child = (
+      <Button
+        onClick={goToFriends}
+        onAuxClick={goToFriendsInAnotherTab}
+        data-testid="yes-button"
+      >
+        {label}
+      </Button>
+    );
+  }
+  return (
+    <div className={classes.wrapper} data-testid="friends-button">
+      {child}
+    </div>
+  );
+};
