6ef86e6be26221be9d7378edce586d1d008a0ec6
anhtudo97 <anhtudo97@gmail.com>
2021-05-31 23:36:05 +0700

    feat: notification repository

---
+++ src/features/notication/data/notification-repository.ts
@@ -0,0 +1,52 @@
+import { Either, left, right } from "fp-ts/Either";
+import NotificationError from "../types/notification-error";
+import { Notification } from "../types/notification";
+import { INotificationAPI } from "./sources/notification-api";
+import { isApolloError } from "@apollo/client";
+
+export interface INotificationRepository {
+  getNotifications(): Promise<Either<NotificationError, Notification[]>>;
+
+  markNotificationAsSeen(
+    notificationID: number
+  ): Promise<Either<NotificationError, boolean>>;
+}
+
+export default class NotificationRepository implements INotificationRepository {
+  private readonly _notificationAPI: INotificationAPI;
+
+  constructor(notificationAPI: INotificationAPI) {
+    this._notificationAPI = notificationAPI;
+  }
+
+  async _leftOrRight<R>(
+    work: () => Promise<R>
+  ): Promise<Either<NotificationError, R>> {
+    try {
+      const result = await work();
+      return right(result);
+    } catch (e) {
+      console.log("NotificationRepo THREW: ", e);
+      if (isApolloError(e)) {
+        const code = e.graphQLErrors[0]?.extensions?.code;
+        if (!code) {
+          // Probably an internet error, not sure
+          return left(NotificationError.network);
+        }
+      }
+      return left(NotificationError.general);
+    }
+  }
+
+  getNotifications(): Promise<Either<NotificationError, Notification[]>> {
+    return this._leftOrRight(() => this._notificationAPI.getNotifications());
+  }
+
+  markNotificationAsSeen(
+    notificationID: number
+  ): Promise<Either<NotificationError, boolean>> {
+    return this._leftOrRight(() =>
+      this._notificationAPI.markNotificationAsSeen(notificationID)
+    );
+  }
+}
