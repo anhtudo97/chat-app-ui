187fbc825958763bb16c41f04784fd6818dd3351
anhtudo97 <anhtudo97@gmail.com>
2021-05-31 23:35:45 +0700

    feat: notification api

---
+++ src/features/notication/data/sources/notification-api.ts
@@ -0,0 +1,73 @@
+import { UpdateBadge } from "./../../../../_generated/UpdateBadge";
+import { GetBadges } from "./../../../../_generated/GetBadges";
+import { Notification, NotificationContent } from "../../types/notification";
+import { ApolloClient } from "@apollo/client";
+import {
+  GetNotifications,
+  GetNotifications_getNotifications,
+} from "../../../../_generated/GetNotifications";
+import { GET_NOTIFICATIONS_QUERY, MARK_NOTIFICATION_AS_SEEN } from "../graphql";
+import { NotificationType } from "../../../../_generated/globalTypes";
+import { UserAPI } from "../../../user/data/sources/user-api";
+import {
+  MarkNotificationAsSeen,
+  MarkNotificationAsSeenVariables,
+} from "../../../../_generated/MarkNotificationAsSeen";
+
+export interface INotificationAPI {
+  getNotifications(): Promise<Notification[]>;
+
+  markNotificationAsSeen(notificationID: number): Promise<boolean>;
+}
+
+export default class NotificationAPI implements INotificationAPI {
+  private readonly _client: ApolloClient<any>;
+
+  constructor(client: ApolloClient<any>) {
+    this._client = client;
+  }
+
+  static _parseContent(
+    notification: GetNotifications_getNotifications
+  ): NotificationContent {
+    switch (notification.type) {
+      case NotificationType.REQUEST_ACCEPTED:
+        return {
+          type: notification.type,
+          user: UserAPI.parserUser(notification.friend!),
+        };
+
+      default:
+        return {
+          type: notification.type,
+          message: "Some system messsage",
+        };
+    }
+  }
+
+  async getNotifications(): Promise<Notification[]> {
+    const { data } = await this._client.query<GetNotifications>({
+      query: GET_NOTIFICATIONS_QUERY,
+      fetchPolicy: "no-cache",
+    });
+    return data.getNotifications.map((n) => {
+      return {
+        id: n.id,
+        date: n.date,
+        seen: n.seen,
+        content: NotificationAPI._parseContent(n),
+      };
+    });
+  }
+
+  async markNotificationAsSeen(notificationID: number): Promise<boolean> {
+    const { data } = await this._client.mutate<
+      MarkNotificationAsSeen,
+      MarkNotificationAsSeenVariables
+    >({
+      mutation: MARK_NOTIFICATION_AS_SEEN,
+      variables: { notificationID },
+    });
+    return data?.markNotificationAsSeen!;
+  }
+}
