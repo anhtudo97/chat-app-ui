c15cc0d5a5c13b00241d8cda9e43359dc622c768
anhtudo97 <anhtudo97@gmail.com>
2021-05-31 23:35:05 +0700

    feat: notification graphql

---
+++ src/features/notication/data/graphql.ts
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+export const GET_NOTIFICATIONS_QUERY = gql`
+  query GetNotifications {
+    getNotifications {
+      id
+      date
+      seen
+      type
+      friend {
+        id
+        username
+        name
+        photoURLSource
+        photoURLMedium
+        photoURLSmall
+      }
+    }
+  }
+`;
+
+export const MARK_NOTIFICATION_AS_SEEN = gql`
+  mutation MarkNotificationAsSeen($notificationID: Int!) {
+    markNotificationAsSeen(notificationID: $notificationID)
+  }
+`;
