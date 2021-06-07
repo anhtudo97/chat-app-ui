18b06758ef61c42ae4ddb2664ad8869d857fded7
anhtudo97 <anhtudo97@gmail.com>
2021-06-07 17:27:46 +0700

    feat: update import

--- src/features/auth/ui/root-screen.tsx
+++ src/features/auth/ui/root-screen.tsx
@@ -1,15 +1,15 @@
 import { shallowEqual } from "react-redux";
 import React, { useCallback, useEffect } from "react";
-// import LoggedInScreen from "../../user/ui/logged-in-screen";
-import LoginScreen from "./login-screen";
+import { LoggedInScreen } from "../../user/ui/logged-in-screen";
+import { LoginScreen } from "./login-screen";
 import { useAuthActions } from "../auth-actions-context";
-// import MeActionsProvider from "../../user/me-actions-context";
+import { MeActionsProvider } from "../../user/me-actions-context";
 import { useAppDispatch, useAppSelector } from "../../../core/redux/hooks";
-import FullscreenLoader from "../../../shared/components/fullscreen-loader";
+import { FullscreenLoader } from "../../../shared/components/fullscreen-loader";
 import AuthError from "../types/auth-error";
-import RetryPage from "../../../shared/components/retry-page";
+import { RetryPage } from "../../../shared/components/retry-page";
 
-const RootScreen = () => {
+export const RootScreen = () => {
   const state = useAppSelector((state) => state.auth, shallowEqual);
   const dispatch = useAppDispatch();
   const { getAccessToken } = useAuthActions();
@@ -26,27 +26,23 @@ const RootScreen = () => {
     const error = state.authError;
     switch (error) {
       case AuthError.network:
-        return "Check your internet connection and try again!";
+        return "Check your internet connection, and try again!";
       default:
-        return "Something went wrong";
+        return "Something weird happened!";
     }
   };
 
   if (!state.initialized) {
-    if (state.authError !== null)
+    if (state.authError != null)
       return <RetryPage onRetry={onRetry} errorMessage={getErrorMessage()} />;
     return <FullscreenLoader />;
   }
-
-  const loggedIn = state.accessToken !== null;
+  const loggedIn = state.accessToken != null;
   if (loggedIn) {
     return (
-      <h1>Logged in</h1>
-      //   <MeActionsProvider>
-      //     <LoggedInScreen />
-      //   </MeActionsProvider>
+      <MeActionsProvider>
+        <LoggedInScreen />
+      </MeActionsProvider>
     );
-  } else {
-    <LoginScreen />;
-  }
+  } else return <LoginScreen />;
 };
