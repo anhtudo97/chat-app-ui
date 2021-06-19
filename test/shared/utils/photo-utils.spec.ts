f7528627f16578f43f148b6ba604d2954ccec355
anhtudo97 <anhtudo97@gmail.com>
2021-06-19 14:43:27 +0700

    chore: update test

---
+++ test/shared/utils/photo-utils.spec.ts
@@ -0,0 +1,24 @@
+import { FileUtils } from "../../../src/shared/utils/file-utils";
+
+const photo = {} as File;
+const url = "photo url";
+
+const photoUtils = new FileUtils();
+
+beforeAll(() => {
+  global.URL = {
+    createObjectURL: jest.fn(() => url),
+  } as unknown as typeof global.URL;
+});
+
+describe("photoToURL", () => {
+  it("should call URL.createObjectURL", async () => {
+    // act
+    const res = await photoUtils.fileToURL(photo);
+
+    // assert
+    expect(global.URL.createObjectURL).toBeCalledWith(photo);
+    expect(global.URL.createObjectURL).toBeCalledTimes(1);
+    expect(res).toBe(url);
+  });
+});
