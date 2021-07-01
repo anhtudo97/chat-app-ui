381264762291458eeb162d73e4202f953846ab38
anhtudo97 <anhtudo97@gmail.com>
2021-07-02 00:14:23 +0700

    feat: test auth ui

--- src/features/badge/ui/components/badges.tsx
+++ src/features/badge/ui/components/badges.tsx
@@ -11,6 +11,110 @@ import { useAppSelector } from "../../../../core/redux/hooks";
 import { NotificationsScreen } from "../../../notification/ui/notifications-screen";
 import { useMobileMQ } from "../../../../shared/styles/media-query";
 
+export const Badges = () => {
+  const badgeState = useAppSelector((state) => state.badge);
+  const friendRequests = useAppSelector(
+    (state) => state.friends.friendRequests
+  );
+  const notifications = useAppSelector(
+    (state) => state.notification.notifications
+  );
+  const [requestCount, setRequestCount] = useState(0);
+  const [notifCount, setNotifCount] = useState(0);
+  const history = useHistory();
+  const [notifAnchor, setNotifAnchor] = useState<HTMLButtonElement | null>(
+    null
+  );
+  const isMobile = useMobileMQ();
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (!badgeState.friendRequests || !friendRequests) {
+      setRequestCount(0);
+    } else {
+      let count = 0;
+      const reqs = friendRequests.received;
+      for (let req of reqs) {
+        if (req.date < badgeState.friendRequests) break;
+        count++;
+      }
+      setRequestCount(count);
+    }
+  }, [badgeState, friendRequests]);
+
+  useEffect(() => {
+    if (!badgeState.notifications || !notifications) {
+      setNotifCount(0);
+    } else {
+      let count = 0;
+      for (let notif of notifications) {
+        if (notif.date < badgeState.notifications) break;
+        count++;
+      }
+      setNotifCount(count);
+    }
+  }, [badgeState, notifications]);
+
+  const notifClicked = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (isMobile) history.push("/notifications", { canGoBack: true });
+      else setNotifAnchor(event.currentTarget);
+    },
+    [history, isMobile]
+  );
+
+  const reqClicked = useCallback(() => {
+    history.push("/requests", { canGoBack: true });
+  }, [history]);
+
+  const closeNotif = useCallback(() => {
+    setNotifAnchor(null);
+  }, [setNotifAnchor]);
+
+  return (
+    <div>
+      <IconButton
+        className={classes.button}
+        onClick={reqClicked}
+        aria-describedby="req"
+        data-testid="friend-requests-button"
+      >
+        <Badge badgeContent={requestCount} className={classes.badge}>
+          <Icon>people</Icon>
+        </Badge>
+      </IconButton>
+      <IconButton
+        className={classes.button}
+        onClick={notifClicked}
+        aria-describedby="notif"
+        data-testid="notifications-button"
+      >
+        <Badge badgeContent={notifCount} className={classes.badge}>
+          <Icon>notifications</Icon>
+        </Badge>
+      </IconButton>
+      <Popover
+        id="notif"
+        open={Boolean(notifAnchor)}
+        anchorEl={notifAnchor}
+        onClose={closeNotif}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <div className={classes.menu}>
+          <NotificationsScreen />
+        </div>
+      </Popover>
+    </div>
+  );
+};
+
 const useStyles = makeStyles({
   button: {
     color: "black",
@@ -33,105 +137,3 @@ const useStyles = makeStyles({
     minHeight: "500px",
   },
 });
-
-export const Badges = () => {
-  const badgeState = useAppSelector((state) => state.badge);
-  const friendRequests = useAppSelector(
-    (state) => state.friends.friendRequests
-  );
-  const notifications = useAppSelector(
-    (state) => state.notification.notifications
-  );
-
-  const [requestCount, setRequestCount] = useState(0);
-  const [notiCount, setNotiCount] = useState(0);
-  const history = useHistory();
-  const [notiAnchor, setNotiAnchor] = useState<HTMLButtonElement | null>(null);
-  const isMobile = useMobileMQ();
-  const classes = useStyles();
-
-  useEffect(() => {
-    if (!badgeState.friendRequests || !friendRequests) {
-      setRequestCount(0);
-    } else {
-      let count = 0;
-      const reqs = friendRequests.received;
-      for (const req of reqs) {
-        if (req.date < badgeState.friendRequests) break;
-        count++;
-      }
-    }
-  }, [badgeState, friendRequests]);
-
-  useEffect(() => {
-    if (!badgeState.notifications || !notifications) {
-      setNotiCount(0);
-    } else {
-      let count = 0;
-      for (let notif of notifications) {
-        if (notif.date < badgeState.notifications) break;
-        count++;
-      }
-      setNotiCount(count);
-    }
-  }, [badgeState, notifications]);
-
-  const notiClicked = useCallback(
-    (event: React.MouseEvent<HTMLButtonElement>) => {
-      if (isMobile) history.push("/notifications", { canGoBack: true });
-      else setNotiAnchor(event.currentTarget);
-    },
-    [history, isMobile]
-  );
-
-  const reqClicked = useCallback(() => {
-    history.push("/requests", { canGoBack: true });
-  }, [history]);
-
-  const closeNoti = useCallback(() => {
-    setNotiAnchor(null);
-  }, [setNotiAnchor]);
-
-  return (
-    <>
-      <IconButton
-        className={classes.button}
-        onClick={reqClicked}
-        aria-describedby="req"
-        data-testid="friend-requests-button"
-      >
-        <Badge badgeContent={requestCount} className={classes.badge}>
-          <Icon>people</Icon>
-        </Badge>
-      </IconButton>
-      <IconButton
-        className={classes.button}
-        onClick={notiClicked}
-        aria-describedby="noti"
-        data-testid="notifications-button"
-      >
-        <Badge badgeContent={notiCount} className={classes.badge}>
-          <Icon>notifications</Icon>
-        </Badge>
-      </IconButton>
-      <Popover
-        id="noti"
-        open={Boolean(notiAnchor)}
-        anchorEl={notiAnchor}
-        onClose={closeNoti}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-      >
-        <div className={classes.menu}>
-          <NotificationsScreen />
-        </div>
-      </Popover>
-    </>
-  );
-};
---
+++ test/features/auth/ui/login-screen.spec.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { AppState, AppStore } from "../../../../src/core/redux/store";
+import { Provider } from "react-redux";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { getMockStore, initialAuthState } from "../../../mock-objects";
+import { AuthActionContext } from "../../../../src/features/auth/auth-actions-context";
+import AuthError from "../../../../src/features/auth/types/auth-error";
+import { LoginScreen } from "../../../../src/features/auth/ui/login-screen";
+import { instance, mock, resetCalls, verify, when } from "ts-mockito";
+import { authActions } from "../../../../src/features/auth/auth-slice";
+
+const MockStore = getMockStore();
+const MockAuthActions = mock<typeof authActions>();
+const signInAction = { type: "signIn" } as any;
+
+const authState = initialAuthState;
+const initialState = {
+  auth: authState,
+} as AppState;
+
+const loginWithGoogleText = "Login with Google";
+
+const Wrapped = ({ mockStore }: { mockStore: AppStore }) => {
+  return (
+    <Provider store={mockStore}>
+      <AuthActionContext.Provider value={instance(MockAuthActions)}>
+        <LoginScreen />
+      </AuthActionContext.Provider>
+    </Provider>
+  );
+};
+
+const renderComponent = (mockStore: AppStore) => {
+  render(<Wrapped mockStore={mockStore} />);
+};
+
+beforeAll(() => {
+  when(MockAuthActions.signInWithGoogle()).thenReturn(signInAction);
+});
+
+beforeEach(() => {
+  resetCalls(MockAuthActions);
+});
+
+test("should contain a login button", () => {
+  // arrange
+  const mockStore = MockStore(initialState);
+  // act
+  renderComponent(mockStore);
+  // assert
+  expect(screen.getByText(loginWithGoogleText)).toBeInTheDocument();
+});
+
+test("Clicking the login button should dispatch the appropriate action", async () => {
+  //arrange
+  const mockStore = MockStore(initialState);
+
+  //act
+  renderComponent(mockStore);
+  fireEvent.click(screen.getByText(loginWithGoogleText));
+  // assert
+  verify(MockAuthActions.signInWithGoogle()).once();
+  expect(mockStore.getActions()).toHaveLength(1);
+  expect(mockStore.getActions()[0]).toBe(signInAction);
+});
+
+test("If the state is loading, should display a spinner instead of the button", () => {
+  // arrange
+  const loadingState: AppState = {
+    ...initialState,
+    auth: {
+      ...authState,
+      loading: true,
+    },
+  };
+  const mockStore = MockStore(loadingState);
+  // act
+  renderComponent(mockStore);
+  expect(screen.queryByText(loginWithGoogleText)).toBeNull();
+  expect(screen.getByTestId("login-spinner")).toBeInTheDocument();
+});
